1a5377759a8d505a85574b350052dd674ca2c659
Lewis_Damy <ldamy16@gmail.com>
2025-08-20 23:30:42 -0300

    feat: basic logic generating pptx working

--- generator/ppt_generator.py
+++ generator/ppt_generator.py
@@ -1,6 +1,38 @@
-import pptx
+from pathlib import Path
+from pptx import Presentation
+
+# Folder containing images
+FOLDER = Path("samples/")
+IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg"]
+
+
+def load_background():
+    """Load the backgrounds to be used in the presentation"""
+    images = [str(f) for f in FOLDER.iterdir() if f.suffix.lower() in IMAGE_EXTENSIONS]
+    if len(images) == 0:
+        print("No images were found")
+        return None
+    return images
+
+prs = Presentation()
 
 
 def create_ppt(content, output_file):
-    # TODO
-    pass
+    print("Creating ppt...")
+
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+
+    title.text = "Hello, World!"
+    subtitle.text = "python-pptx was here!"
+
+    images = load_background()
+
+    print(type(slide.background))
+
+    left = top = 0
+    slide.shapes.add_picture(images[0], left-0.1*prs.slide_width, top, height = prs.slide_height)
+
+    prs.save(output_file)
